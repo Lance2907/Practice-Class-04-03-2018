718bdfeda804dc25ba211d08d2f42262a35fd3bf
Latta <lslatta.2015@mymail.jcsu.edu>
2018-04-03 13:15:39 -0400

    first commit

---
+++ Addition.java
@@ -0,0 +1,13 @@
+
+public class Addition {
+public static void main (String[]args){
+	int []x={2,3,4,};
+	int[] y={6,7,8};
+	int [] sum =new int[3];
+	for (int z=0; z<x.length;z++)
+	{sum[z]=x[z]+y[z];
+	System.out.println(sum[z]);
+	}
+}
+}
+
